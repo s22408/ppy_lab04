6b0c20c03dc953308545958bcbec30287c5370b0
s22408 <129112889+s22408@users.noreply.github.com>
2023-04-04 11:35:36 +0200

    final update

--- zad3.py
+++ zad3.py
@@ -11,3 +11,27 @@
 #poza zakres listy  z alfabetem oraz problem podania klucza
 #o dowolnej wielkości(20%).
 #Funkcja opcjonalnie przyjmuje dowolny alfabet. Domyślnie używa angielskiego(10%).
+#alfabet: 'abcdefghijklmnopqrstuvwxyz'
+def function3(text, key):
+
+    wiadomosc = text.lower()
+    klucz = key%26              #rozwiazanie problemu duzego klucza
+    wynik = ''
+
+    for x in range(len(wiadomosc)):
+
+        if ord(wiadomosc[x])<97 or ord(wiadomosc[x])>122:
+            wynik = wynik + wiadomosc[x]
+            continue
+
+        nowyZnak = ord(wiadomosc[x])+klucz
+
+        if nowyZnak>122:
+            nowyZnak = nowyZnak - 122 + 96 #bo zakres znakow od 97(a) do 122(z)
+
+        wynik = wynik + chr(nowyZnak)
+
+    return wynik
+
+#test
+print(function3('zzz aaa !@#$%^& bbbb',27))
